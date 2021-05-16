2ba2197b97c101167f06b2e3c79fa5a005766151
rhalm <49129049+rhalm@users.noreply.github.com>
2021-05-16 19:06:31 +0200

    Added JaCoCo, increased code coverage

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -63,6 +63,59 @@ public class GT4500Test {
     verify(store2, times(1)).isEmpty();
   }
 
+  
+  @Test
+  public void fireTorpedo_Single_Twice_Full_Success(){
+    // Arrange
+    when(store1.fire(1)).thenReturn(true);
+    when(store2.fire(1)).thenReturn(true);
+
+    // Act
+    ship.fireTorpedo(FiringMode.SINGLE);
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(true, result);
+    verify(store1, times(1)).fire(1);
+    verify(store2, times(1)).fire(1);
+  }
+
+    @Test
+  public void fireTorpedo_Single_Twice_Half_Success(){
+    // Arrange
+    when(store1.fire(1)).thenReturn(true);
+    when(store2.isEmpty()).thenReturn(true);
+
+    // Act
+    ship.fireTorpedo(FiringMode.SINGLE);
+    
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(true, result);
+    verify(store1, times(2)).fire(1);
+    verify(store2, times(1)).isEmpty();
+  }
+
+      @Test
+  public void fireTorpedo_Single_Twice_Failure(){
+    // Arrange
+    when(store1.fire(1)).thenReturn(true);
+    when(store2.isEmpty()).thenReturn(true);
+    
+    // Act
+    ship.fireTorpedo(FiringMode.SINGLE);
+    when(store1.isEmpty()).thenReturn(true);
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(false, result);
+    verify(store1, times(1)).fire(1);
+    verify(store1, times(2)).isEmpty();
+    verify(store2, times(1)).isEmpty();
+  }
+
+
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
@@ -82,7 +135,7 @@ public class GT4500Test {
   public void fireTorpedo_All_Failure(){
     // Arrange
     when(store1.isEmpty()).thenReturn(true);
-    
+
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
@@ -90,5 +143,44 @@ public class GT4500Test {
     assertEquals(false, result);
     verify(store1, times(1)).isEmpty();
   }
+
+  @Test
+  public void fireTorpedo_All_Second_Empty_Failure(){
+    // Arrange
+    when(store2.isEmpty()).thenReturn(true);
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.ALL);
+
+    // Assert
+    assertEquals(false, result);
+    verify(store1, times(1)).isEmpty();
+    verify(store2, times(1)).isEmpty();
+  }
+
+  @Test
+  public void fireTorpedo_All_Full_Failure(){
+    // Arrange
+    when(store2.fire(1)).thenReturn(false);
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.ALL);
+
+    // Assert
+    assertEquals(false, result);
+  }
+
+  @Test
+  public void fireTorpedo_All_Full1_Failure(){
+    // Arrange
+    when(store1.fire(1)).thenReturn(false);
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.ALL);
+
+    // Assert
+    assertEquals(false, result);
+  }
+
 
 }
