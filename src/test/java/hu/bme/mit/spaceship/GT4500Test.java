9269d76b57a2c0ed25b111b2e638c3c8451a0115
rhalm <49129049+rhalm@users.noreply.github.com>
2021-05-16 18:13:14 +0200

    Added isolation for tests with Mockito

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -8,33 +8,39 @@ import static org.mockito.Mockito.*;
 
 public class GT4500Test {
 
+  private TorpedoStore store = mock(TorpedoStore.class);
   private GT4500 ship;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    this.ship = new GT4500(store, store);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    // Set the behavior of the mock: if it is called with
+    // parameter "1" then return true.
+    when(store.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+    verify(store, times(1)).fire(1);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
-
+    when(store.fire(1)).thenReturn(true);
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
+    verify(store, times(2)).fire(1);
   }
 
 }
